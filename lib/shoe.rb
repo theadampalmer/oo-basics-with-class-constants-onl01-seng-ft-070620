1501b0d6d1dc1f4d75f700028e3e1b300d0d0ff1
Adam Palmer <theadampalmer@gmail.com>
2020-07-16 22:19:24 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,7 @@
 require 'pry'
- class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+
+class Shoe
+    attr_accessor :color, :size, :material, :condition
+    attr_reader :brand
+    
+    
